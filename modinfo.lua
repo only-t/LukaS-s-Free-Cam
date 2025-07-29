0e3e218dde03c9d0f876fe55b5bed31c29d318f4
Luкaട <onlyt.coding@gmail.com>
2025-07-29 19:21:05 +0200

    Updated mods description.

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,13 @@
 name = "LukaS's Free Cam"
-description = "A CAM!? FOR FREE!?"
+description = [[
+A CAM!? FOR FREE!?
+
+This mod allows you to switch between your regular Don't Starve camera and a free fly camera with no restrictions on where you can or can't go!
+
+The default camera toggle keybind is '=' but this can be changed inside the games settings (NOT MOD CONFIGURATIONS, the actual game settings).
+
+This mod does not support controllers, yet...
+]]
 author = "LukaS"
 version = "1.0.0"
 forumthread = "/profile/365042-lukas/"
